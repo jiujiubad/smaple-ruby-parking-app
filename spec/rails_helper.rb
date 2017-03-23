b8052509c8e40e7b51b834ae8d2e2aa03c8bfa40
Wen-Tien Chang <ihower@gmail.com>
2017-03-23 19:11:16 +0800

    Support focus

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,9 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :feature
 
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
